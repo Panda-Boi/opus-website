afa5de9323b5f6762c7b2f626ef68696675f0d2c
Nihar <niharputhink@gmail.com>
2021-05-01 14:00:20 +0530

    added basic sign up and log in logic

--- main/urls.py
+++ main/urls.py
@@ -6,6 +6,9 @@ app_name = 'main'
 urlpatterns = [
     path("", views.index, name="index"),
     path("organisations", views.organisations, name="organisations"),
-    path("signUp", views.signUp, name="signUp"),
-    path("aboutUs", views.aboutUs, name="aboutUs")
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("aboutUs", views.aboutUs, name="aboutUs"),
+    path("account", views.user, name="user"),
+    path("signUp", views.signUp, name="signUp")
 ]
--- main/views.py
+++ main/views.py
@@ -1,15 +1,60 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
     return render(request, 'main/index.html')
 
+
 def organisations(request):
     return HttpResponse('In progress')
 
-def signUp(request):
-    return HttpResponse('In progress')
+def login_view(request):
+    if request.method == "POST":
+        # Accessing username and password from form data
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        # Check if username and password are correct, returning User object if so
+        user = authenticate(request, username=username, password=password)
+
+        # If user object is returned, log in and route to index page:
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(reverse("main:index"))
+        # Otherwise, return login page again with new context
+        else:
+            return render(request, "main/login.html", {
+                "message": "Invalid Credentials"
+            })
+    
+    return render(request, 'main/login.html')
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("main:index"))
 
 def aboutUs(request):
     return HttpResponse('In progress')
+
+def user(request):
+    return render(request, 'main/account.html')
+
+def signUp(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        password2 = request.POST["password2"]
+        email = request.POST["email"]
+
+        if password == password2:
+            user = User.objects.create_user(username, email, password)
+            return HttpResponseRedirect(reverse("main:login"))
+        else:
+            return render(request, 'main/signUp.html', {
+                'message': 'Passwords do not match'
+            })
+
+    return render(request, 'main/signUp.html')
